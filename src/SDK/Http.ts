56901a9f6f6ffb225f436711b6f6e9e80d6171b6
Meezaan-ud-Din Abdu Dhil-Jalali Wal-Ikram <m@meezaan.net>
2023-06-12 16:11:38 +0400

    Make base url customisable for internal use and testing

--- src/SDK/Http.ts
+++ src/SDK/Http.ts
@@ -1,9 +1,10 @@
 export default class Http {
-    baseurl: string = 'https://api.7x.ax';
-    useragent: string = '7x/js/1.0.11';
+    baseurl: string;
+    useragent: string = '7x/js/1.0.12';
     apikey: string;
 
-    constructor(apikey: string) {
+    constructor(apikey: string, baseurl: string = 'https://api.7x.ax') {
         this.apikey = apikey;
+        this.baseurl = baseurl;
     }
 }
